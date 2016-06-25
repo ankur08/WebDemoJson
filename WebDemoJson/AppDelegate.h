9a114c626d3a556ee169a63e27ab72fa43734d9c
ankur <ankur@ankurs-mac.local>
2016-06-26 01:48:09 +0530

    Initial Commit

---
+++ WebDemoJson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebDemoJson
+//
+//  Created by ankur on 26/06/16.
+//  Copyright © 2016 ankur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebDemoJson/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebDemoJson
+//
+//  Created by ankur on 26/06/16.
+//  Copyright © 2016 ankur. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
